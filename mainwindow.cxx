6945bcdaaeac290f466a4554cc8378347581f125
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-08-21 00:01:10 +0300

    added blackmagic serial ports opening and basic logging

--- mainwindow.cxx
+++ mainwindow.cxx
@@ -1,6 +1,8 @@
 #include "mainwindow.hxx"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::MainWindow)
@@ -9,6 +11,17 @@ MainWindow::MainWindow(QWidget *parent) :
 	gdbserver.listen(QHostAddress::Any, 2331);
 	connect(& gdbserver, SIGNAL(newConnection()), this, SLOT(newConnection()));
 	gdbserver_socket = 0;
+	bm_gdb_port.setPortName("com6");
+	bm_debug_port.setPortName("com7");
+	
+	if (!bm_gdb_port.open(QIODevice::ReadWrite))
+		QMessageBox::critical(0, "error", "could not open blackmagic gdb port");
+	else
+		QMessageBox::information(0, "success", "blackmagic gdb port opened successfully");
+	if (!bm_debug_port.open(QIODevice::ReadWrite))
+		QMessageBox::critical(0, "error", "could not open blackmagic debug port");
+	else
+		QMessageBox::information(0, "success", "blackmagic debug port opened successfully");
 }
 
 MainWindow::~MainWindow()
@@ -24,4 +37,5 @@ void MainWindow::newConnection()
 
 void MainWindow::gdbsocketReadyRead()
 {
+	ui->plainTextEditGdbserverLog->appendPlainText(gdbserver_socket->readAll());
 }
--- mainwindow.hxx
+++ mainwindow.hxx
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QtSerialPort/QSerialPort>
 
 namespace Ui {
 class MainWindow;
@@ -25,6 +26,9 @@ private:
 	Ui::MainWindow *ui;
 	QTcpServer	gdbserver;
 	QTcpSocket	* gdbserver_socket;
+	/* blackmagic usb serial ports */
+	QSerialPort	bm_gdb_port;
+	QSerialPort	bm_debug_port;
 };
 
 #endif // MAINWINDOW_HXX
